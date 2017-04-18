e3b1c0061077a8de7658b08b0ecd950ba6b5c461
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-18 11:32:28 -0500

    Mejorando css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,20 @@
-var files = ["assets/images/imagen1.jpg","assets/images/imagen2.jpg","assets/images/imagen3.jpg","assets/images/imagen4.jpg"]
+var files = [{file:"assets/images/imagen1.jpg" , alt:"pareja panda"},{file:"assets/images/imagen2.jpg" , alt:"bebe panda" }
+            ,{file:"assets/images/imagen3.jpg" , alt:"familia panda"},{file:"assets/images/imagen4.jpg" , alt:"panda comiendo" }]
 var docFragment = document.createDocumentFragment();
 var parentPictures = document.getElementById("pictures-panda");
 
-function createImages(src) {
+function createImages(src,alt) {
   var content = document.createElement("div");
   var images = document.createElement("img");
   var close = document.createElement("a");
+  var textClose = document.createTextNode("x");
 
   images.src = src;
+  images.alt=alt;
   close.setAttribute("class","close");
-  close.innerHTML="x";
 
   close.addEventListener("click",function(e) {
     var parent = e.target.parentNode;
-    alert(parent.getAttribute("class"));
     parent.removeChild(images);
     parent.removeChild(close);
     parent.parentNode.removeChild(parent);
@@ -22,38 +23,32 @@ function createImages(src) {
   docFragment.appendChild(content);
   content.appendChild(images);
   content.appendChild(close);
+  close.appendChild(textClose);
   return docFragment;
 }
 
-function mostrarOcultar(elemento) {
-  if (elemento.style.display == "block") {
-    elemento.style.display =  "none";
-  }
-  else{
-    elemento.style.display = "block";
-  }
-}
 window.addEventListener("load",function () {
   files.forEach(function(e) {
-    parentPictures.appendChild(createImages(e));
+    parentPictures.appendChild(createImages(e.file,e.alt));
   })
 });
 
-var restore = document.getElementsByClassName("restaurar")[0];
-restore.addEventListener("click",function () {
+var buttonMenu = document.getElementsByClassName("button-menu");
+buttonMenu[2].addEventListener("click",function () {
   files.forEach(function(e) {
-    parentPictures.appendChild(createImages(e));
+    parentPictures.appendChild(createImages(e.file,e.alt));
   })
 })
 
-var contentHistory = document.getElementsByClassName("historia-panda")[0];
-document.getElementsByClassName("historia")[0].addEventListener("click",function (e) {
+var contentHistory = document.getElementById("historia-panda");
+// var n = document.getElementById("historia");
+buttonMenu[1].addEventListener("click",function (e) {
   e.preventDefault();
-  mostrarOcultar(contentHistory);
+  document.getElementById("historia-panda").classList.toggle("ocultar");
 })
 
-var origen = document.getElementsByClassName("origen-panda")[0];
-document.getElementsByClassName("origen")[0].addEventListener("click",function (e) {
+var origen = document.getElementById("origen-panda");
+buttonMenu[0].addEventListener("click",function (e) {
   e.preventDefault();
-  mostrarOcultar(origen);
+  origen.classList.toggle("ocultar");
 })
